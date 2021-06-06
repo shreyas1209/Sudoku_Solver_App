5ebd8c09d4d08fbcf4d23a10e4d937e0a74e956c
Shreyas Sesham <55966833+shreyas1209@users.noreply.github.com>
2021-06-06 23:56:17 +0530

    Update sudoku_detector.py

--- sudoku_detector.py
+++ sudoku_detector.py
@@ -45,59 +45,21 @@ def find_boundary(img,show = True):
   contours = sorted(contours, key=cv2.contourArea, reverse=True)
   sudoku_box = contours[0]
   
-
  
 	# Top-left has point smallest (x + y) value
 	# Top-right point has largest (x - y) value
   # Bottom-left point has smallest (x - y) value
-  # Bottom-right point has largest (x - y) value
+  # Bottom-right point has largest (x + y) value
 	
+  #idea taken from aakash jhawar's blog
   top_left , _ = min(enumerate([tl[0][0] + tl[0][1] for tl in sudoku_box]), key= operator.itemgetter(1))
-  top_right , _ = min(enumerate([tr[0][0] + tr[0][1] for tr in sudoku_box]), key= operator.itemgetter(1))
-  bottom_left , _ = min(enumerate([bl[0][0] + bl[0][1] for bl in sudoku_box]), key= operator.itemgetter(1))
-  bottom_right , _ = min(enumerate([br[0][0] + br[0][1] for br in sudoku_box]), key= operator.itemgetter(1))
-  print(top_left,top_right,bottom_left,bottom_right)
-
-
-
-
-
-
+  top_right , _ = max(enumerate([tr[0][0] - tr[0][1] for tr in sudoku_box]), key= operator.itemgetter(1))
+  bottom_left , _ = min(enumerate([bl[0][0] - bl[0][1] for bl in sudoku_box]), key= operator.itemgetter(1))
+  bottom_right , _ = max(enumerate([br[0][0] - br[0][1] for br in sudoku_box]), key= operator.itemgetter(1))
   
-  '''for i,contour in enumerate(contours):
-    approx_box = cv2.approxPolyDP(contour, 0.01*cv2.arcLength(contour,True),True)
-    if len(approx_box)==4:
-      X,Y,W,H = cv2.boundingRect(approx_box)
-      
-      if (H>3 and W>3):
-        cv2.drawContours(original_pic, [approx_box], 0, (0,0,255),5)
-        cv2_imshow(original_pic)
-        puzzle = four_point_transform(img, approx_box.reshape(4, 2))
+  return(sudoku_box[top_left][0],sudoku_box[top_right][0],sudoku_box[bottom_left][0],sudoku_box[bottom_right][0])
 
 
-        if show == True:
-          cv2_imshow(image_gray)
-          cv2_imshow(image_blur)
-          cv2_imshow(image_threshold)
-          cv2_imshow(image_processed)
-          cv2_imshow(puzzle)
-
-          contours = cv2.findContours(image_processed.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-          contours = imutils.grab_contours(contours)
-          contours = sorted(contours, key=cv2.contourArea, reverse=True)
-          largest_contour = contours[0]
-
-  return(puzzle)'''
-
-'''
-digit_extraction(img,position,img_size,grid_size, show = False):
-img is the image
-position is the tuple for the position in the grid (zero_indexed)
-img_size is the tuple for the image size()
-grid_size is the tuple for the size of the grid (like (9,9))
-extracted_img_size is the tuple for the size for the extracted image
-All tuples are of the form(height,width)
-'''
 
 from skimage.segmentation import clear_border
 def digit_extraction(img,position,img_size,grid_size,extracted_img_size,show = False):
@@ -119,6 +81,11 @@ def digit_extraction(img,position,img_size,grid_size,extracted_img_size,show = F
     cv2_imshow(digit_threshold)
 
   return(digit_threshold)
+
+        
+      
+
+  
 
         
       
