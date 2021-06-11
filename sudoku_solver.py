a337be08f8646fc8aba104005eb45a08cca97a96
Shreyas Sesham <55966833+shreyas1209@users.noreply.github.com>
2021-06-11 23:26:58 +0530

    Update sudoku_solver.py

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,21 +1,28 @@
+#pass grids as a np array
+import numpy as np
 def row_check(grid,row,num):
-    for column in range(9):
-        if grid[row][column]==num:
-            return True
-    return False
+    row_values = grid[row]
+    row_items  = np.unique(row_values)
+    if num in row_items:
+        return True
+    else:
+        return False
 
 def col_check(grid,column,num):
-    for row in range(9):
-        if grid[row][column]==num:
-            return True
-    return False
+    col_values = (np.transpose(grid))[column]
+    col_items = np.unique(col_values)
+    if num in col_items:
+        return True
+    else:
+        return False
 
 def box_check(grid,num,box_row_start,box_col_start):
-    for row in range(box_row_start,box_row_start+3):
-        for col in range(box_col_start,box_col_start+3):
-            if grid[row][column]==num:
-                return True
-    return False
+    sub_grid = grid[box_row_start:box_row_start+3,box_col_start:box_col_start+3]
+    sub_grid_values = np.unique(sub_grid)
+    if num in sub_grid_values:
+        return True
+    else:
+        return False
 
 
 
