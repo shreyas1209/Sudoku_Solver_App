d858b64219af96c3fcce8b23f73e62a8490b1cf0
Shreyas Sesham <55966833+shreyas1209@users.noreply.github.com>
2021-06-12 00:31:25 +0530

    Update sudoku_solver.py

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -23,6 +23,45 @@ def box_check(grid,num,box_row_start,box_col_start):
         return True
     else:
         return False
+def find_empty(grid):
+  indices = np.where(grid == 0)
+  rows,cols = indices[0],indices[1]
+
+  if (((len(rows))==0) and ((len(cols))==0)):
+    return False
+  else:
+    row = rows[0]
+    col = cols[0]
+    return (True,row,col)
+
+def valid_place(grid,row,col,num) :
+  print((not(row_check(grid,row,num))))
+  print((not(col_check(grid,col,num))))
+  print((not(box_check(grid,(row - (row%3)) ,(col - (col%3)), num))))
+  return((not(row_check(grid,row,num))) and(not(col_check(grid,col,num))) and (not(box_check(grid,(row - (row%3)) ,(col - (col%3)), num))))
+
+
+def solve_sudoku(grid):
+  empty_bool,empty_row,empty_col = find_empty(grid)
+  if (empty_bool ==False):
+    return True
+  for n in range(1,10):
+    print(valid_place(grid,empty_row,empty_col,n))
+    if (valid_place(grid,empty_row,empty_col,n)==True):
+      grid[empty_row][empty_col] = n
+      print(grid)
+      if (solve_sudoku(grid) == True):
+        return True
+      grid[empty_row][empty_col] = 0
+  print(grid)
+  return False
+
+      
+
+
+
+
+
 
 
 
