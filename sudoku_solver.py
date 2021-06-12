7e676b50d06dc5ff2c3251d15b92d66d5f2d342a
Shreyas Sesham <55966833+shreyas1209@users.noreply.github.com>
2021-06-12 22:54:50 +0530

    Update sudoku_solver.py

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -28,33 +28,30 @@ def find_empty(grid):
   rows,cols = indices[0],indices[1]
 
   if (((len(rows))==0) and ((len(cols))==0)):
-    return False
+    return (False,None,None)
   else:
     row = rows[0]
     col = cols[0]
     return (True,row,col)
 
 def valid_place(grid,row,col,num) :
-  print((not(row_check(grid,row,num))))
-  print((not(col_check(grid,col,num))))
-  print((not(box_check(grid,(row - (row%3)) ,(col - (col%3)), num))))
-  return((not(row_check(grid,row,num))) and(not(col_check(grid,col,num))) and (not(box_check(grid,(row - (row%3)) ,(col - (col%3)), num))))
+  return((not(row_check(grid,row,num))) and(not(col_check(grid,col,num))) and (not(box_check(grid,num,(row - (row%3)) ,(col - (col%3))))))
 
 
 def solve_sudoku(grid):
   empty_bool,empty_row,empty_col = find_empty(grid)
   if (empty_bool ==False):
-    return True
+    return (True,grid)
   for n in range(1,10):
-    print(valid_place(grid,empty_row,empty_col,n))
     if (valid_place(grid,empty_row,empty_col,n)==True):
       grid[empty_row][empty_col] = n
-      print(grid)
-      if (solve_sudoku(grid) == True):
-        return True
+      if ((solve_sudoku(grid)[0]) == True):
+        return (True,grid)
       grid[empty_row][empty_col] = 0
-  print(grid)
-  return False
+  
+  return (False,None)
+
+
 
       
 
